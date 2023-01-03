7d15571d232ad32201801da1531ea92933210692
Diego Araújo <brhdn050654@ambev.com.br>
2023-01-03 09:33:34 -0300

    Add dashboard

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,12 +1,19 @@
-import { type } from "os";
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
+import { useRouter } from "next/router";
+import { api } from "../services/api";
 
+type User = {
+  email: string;
+  permissions: string[];
+  roles: string[];
+};
 type SigInCredentials = {
   email: string;
   password: string;
 };
 type AuthContextData = {
   sigIn(credentials: SigInCredentials): Promise<void>;
+  user: User | undefined;
   isAuthenticated: boolean;
 };
 type AuthProviderProps = {
@@ -16,13 +23,22 @@ type AuthProviderProps = {
 export const AuthContext = createContext({} as AuthContextData);
 
 export function AuthProvider({ children }: AuthProviderProps) {
-  const isAuthenticated = false;
+  const router = useRouter();
+  const [user, setUser] = useState<User>();
+  const isAuthenticated = !!user;
 
   async function sigIn({ email, password }: SigInCredentials) {
-    console.log({ email, password });
+    try {
+      const response = await api.post("sessions", { email, password });
+      const { permissions, roles } = response.data;
+      setUser({ email, permissions, roles });
+      router.push("/dashboard");
+    } catch (err) {
+      console.log(err);
+    }
   }
   return (
-    <AuthContext.Provider value={{ sigIn, isAuthenticated }}>
+    <AuthContext.Provider value={{ sigIn, isAuthenticated, user }}>
       {children}
     </AuthContext.Provider>
   );
