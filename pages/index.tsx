48639b947586dc9dc4bd6afbf5bf8793e88e8a7a
Diego Araújo <brhdn050654@ambev.com.br>
2023-01-04 10:10:17 -0300

    Add redirect in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
+import { GetServerSideProps } from "next";
 import { FormEvent, useContext, useState } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 import styles from "../styles/Home.module.css";
+import { parseCookies } from "nookies";
 
 export default function Home() {
   const [email, setEmail] = useState("");
@@ -31,3 +33,20 @@ export default function Home() {
     </form>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const cookies = parseCookies(ctx);
+
+  if (cookies["nextauth.token"]) {
+    return {
+      redirect: {
+        destination: "/dashboard",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
