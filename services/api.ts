eb3d0c8cc1e92c9e5128831aa8de6e7f4a478ef8
Diego Araújo <brhdn050654@ambev.com.br>
2023-01-04 09:43:21 -0300

    Fix api

--- services/api.ts
+++ services/api.ts
@@ -3,7 +3,7 @@ import { parseCookies, setCookie } from "nookies";
 
 let cookies = parseCookies();
 let isRefreshing = false;
-let failedRequestsQueue = [];
+let failedRequestsQueue: any = [];
 
 export const api = axios.create({
   baseURL: "http://localhost:3333",
@@ -27,23 +27,42 @@ api.interceptors.response.use(
         if (!isRefreshing) {
           isRefreshing = true;
 
-          api.post("/refresh", { refreshToken }).then((response) => {
-            const { token } = response.data;
-            setCookie(undefined, "nextauth.token", token, {
-              maxAge: 60 * 60 * 24 * 30, // 30 days
-              path: "/",
-            });
-            setCookie(
-              undefined,
-              "nextauth.refreshToken",
-              response.data.refreshToken,
-              {
+          api
+            .post("/refresh", {
+              refreshToken,
+            })
+            .then((response) => {
+              console.log("Atualizando token");
+              const { token } = response.data;
+              setCookie(undefined, "nextauth.token", token, {
                 maxAge: 60 * 60 * 24 * 30, // 30 days
                 path: "/",
-              }
-            );
-            api.defaults.headers["Authorization"] = `Bearer ${token}`;
-          });
+              });
+              setCookie(
+                undefined,
+                "nextauth.refreshToken",
+                response.data.refreshToken,
+                {
+                  maxAge: 60 * 60 * 24 * 30, // 30 days
+                  path: "/",
+                }
+              );
+              api.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+              failedRequestsQueue.forEach((request: any) =>
+                request.onSuccess(token)
+              );
+              failedRequestsQueue = [];
+            })
+            .catch((err) => {
+              failedRequestsQueue.forEach((request: any) =>
+                request.onFailure(err)
+              );
+              failedRequestsQueue = [];
+            })
+            .finally(() => {
+              isRefreshing = false;
+            });
         }
         return new Promise((resolve, reject) => {
           failedRequestsQueue.push({
